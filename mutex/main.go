9e29587709db9b390a81568256add03204786191
Loc Educate <loceducate@gmail.com>
2022-11-14 22:53:58 +0700

    mutex

---
+++ mutex/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var count int = 0
+	lock := new(sync.RWMutex)
+
+	for i := 1; i <= 5; i++ {
+		go func() {
+			for i := 1; i <= 10000; i++ {
+				lock.Lock()
+				count += 1
+				lock.Unlock()
+			}
+		}()
+	}
+
+	time.Sleep(time.Second * 7)
+	fmt.Println("Count", count)
+}
